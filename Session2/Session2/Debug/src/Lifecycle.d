800440190fad29ead4b73fda586ff3320dd161b7
Hirsch Gerd (CC-AD/ESW1) <gerd.hirsch@de.bosch.com>
2021-01-26 11:55:41 +0100

    reorg repo

--- Session2/Session2/Debug/src/Lifecycle.d
+++
@@ -1,3 +0,0 @@
-src/Lifecycle.o: ../src/Lifecycle.cpp ../src/A1Empty.hpp
-
-../src/A1Empty.hpp:
--- Session4/Session4/Debug/src/ParameterAndReturnExpressions.d
+++
@@ -1,4 +0,0 @@
-src/ParameterAndReturnExpressions.o: \
- ../src/ParameterAndReturnExpressions.cpp ../src/A2.h
-
-../src/A2.h:
